6b6a8a5e1969b1fcacbdc13e5f2ac101a3e84432
daotan.liu <liudaotan@foxmail.com>
2023-04-02 11:09:58 +0800

    add test img

--- detect_and_compare.py
+++ detect_and_compare.py
@@ -4,8 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
-debug = 0
-
+debug = 2
 
 class colorDecetection:
     def __init__(self, img, net, sample_num, kernel_size=(10, 10)):
@@ -34,8 +33,8 @@ class colorDecetection:
         img_bool = ((img > 0.1) * 1).astype('uint8')
         img_fill = self.fill_hole(img_bool)
 
-        if debug ==2:
-            show_array(img_bool*255)
+        if debug == 2:
+            # show_array(img_bool*255)
             show_array(img_fill)
 
         # 计算轮廓
@@ -85,7 +84,7 @@ class colorDecetection:
             masks.append(cv2.drawContours(mask, [contour], 0, (1, 1, 1), cv2.FILLED))
         masks = [mask[:, :, 0] for mask in masks]
 
-        if debug:
+        if debug == 1:
             # show 经过筛选后的 mask
             mask_combine = np.zeros_like(masks[0])
             for m in masks:
@@ -188,7 +187,7 @@ def main(img_path, sample_num, target_num):
     img_original = cv2.imread(img_path)
     size = img_original.shape
 
-    # 将图片分为上下两部分
+    # split the original img to two parts
     h = size[0]
     img_stand = img_original[:h // 2]
     img_test = img_original[h // 2:]
@@ -198,7 +197,7 @@ def main(img_path, sample_num, target_num):
     net = cv2.dnn.readNet(cv2.samples.findFile(r'./pretrained_model/deploy.prototxt'),
                           cv2.samples.findFile(r'pretrained_model/hed_pretrained_bsds.caffemodel'))
 
-    # 分别检测上下两部分的目标并计算每个目标的直方图
+    # calculate the histogram of the two parts
     stand_group = colorDecetection(img_stand, net, sample_num)
     target_group = colorDecetection(img_test, net, target_num)
 
@@ -213,7 +212,7 @@ if __name__ == '__main__':
     # img_name = '5CAC56C7DABA6CD0213279A6213323FE.png'  #
     # img_name = '5EDCB0038E7C7315990997B80CD90ACF.png'  # 成功
     # img_name = '5F0E7ABFB3DD3C07A7FE83535DD93333.png'  # 成功
-    # img_name = '6B8B7C98EE3522AB8036CD239980E62B.png'  # 反光 failed
+    # img_name = '6B8B7C98EE3522AB8036CD239980E62B.png'  # 反光
     # img_name = '7BA24FF89E790157089F4A7536FF10B2.png'  # 成功
     # img_name = '7D7A43EF00E9CD6784032347011A6101.png'  # 成功
     # img_name = '56BF8DF8922FF549443E283E7A3DF92C.png'  # 成功
@@ -224,7 +223,10 @@ if __name__ == '__main__':
     # img_name = 'B1D77DAA0C1C63DBA67A7730B86B6A99.png'  #
     # img_name = 'D102A54301657C84862B3A2CDD295A4B.png'
     # img_name = 'DC001EE63CDA633989A8E25961F19601.png'
-    img_name = 'EA8BA058FECAF9A463BF33E1AE2CCCF2.png'
+    # img_name = 'EA8BA058FECAF9A463BF33E1AE2CCCF2.png'
+    # img_name = 'B1D77DAA0C1C63DBA67A7730B86B6A99.png'
+    img_name = '848A3E6B82EDA42451DBF4AE5A60A000.png'
+    img_name = '1111_20230402105500.jpg'
 
     img_path = './data/' + img_name
     main(img_path, sample_num=7, target_num=7)
